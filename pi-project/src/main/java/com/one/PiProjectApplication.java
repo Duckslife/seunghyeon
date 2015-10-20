8f8875442ce954154b758b3ba6e0042c226ea69b
jshsakura <jshsakura@naver.com>
2015-10-21 04:02:45 +0900

    테스트

--- pi-project/src/main/java/com/one/PiProjectApplication.java
+++ pi-project/src/main/java/com/one/PiProjectApplication.java
@@ -4,24 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.one.dao.SosiRepository;
-import com.one.models.Sosi;
-
 @SpringBootApplication
 public class PiProjectApplication {
 
     public static void main(String[] args) {
         //SpringApplication.run(PiProjectApplication.class, args);
         ConfigurableApplicationContext context = SpringApplication.run(PiProjectApplication.class, args);
-        SosiRepository sosiRepository = context.getBean(SosiRepository.class);
-        sosiRepository.save(new Sosi("태연"));
-        sosiRepository.save(new Sosi("윤아"));
-        sosiRepository.save(new Sosi("수영"));
-        sosiRepository.save(new Sosi("효연"));
-        sosiRepository.save(new Sosi("유리"));
-        sosiRepository.save(new Sosi("티파니"));
-        sosiRepository.save(new Sosi("써니"));
-        sosiRepository.save(new Sosi("서현"));
+        //BoardDao boardRepository = context.getBean(BoardDao.class);
+        //boardRepository.save(new Board("")); 생성자를 이용한 데이터 미리입력 가능을 위한 주석
+
         
     }
 }
